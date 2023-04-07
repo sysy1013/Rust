40fe3071b971137b73cda0f035e61410cb3d62d7
sysy1013 <tlgud2325@naver.com>
2023-04-07 18:08:34 +0900

    [ADD] Emptytuple, Debus print

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-fn give_age() -> i32 {
+/*fn give_age() -> i32 {
     26
-}
+}*/
+fn empty_tuple() {}
 fn main() {
-    let my_name = "SiHyeong";
+    let tuple = empty_tuple();
+    println!("{:?}", tuple);
+    // {} -> Display {:?} -> Debug print
+
+    /* let my_name = "SiHyeong";
     //let my_age = 26;
     println!("My name is {my_name} and my age is {}", give_age());
 
@@ -18,7 +23,7 @@ fn main() {
     println!(
         "The city of {0} in {1} had a population of {2}",
         my_city, year, population
-    );
+    );*/
 }
 //macro = 코드를 쓰는 함수.
 //{}안에서 expression 허용 x 뒤에서는 가능, 즉 1+5 못함
